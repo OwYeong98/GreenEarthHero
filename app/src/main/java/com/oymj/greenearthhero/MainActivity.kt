16fd257dfb122fa82e304c37fac4fd32315441fc
OwYeong98 <mun-ji@hotmail.com>
2020-02-23 19:05:17 +0800

    fix some UI problem

--- app/src/main/java/com/oymj/greenearthhero/MainActivity.kt
+++ app/src/main/java/com/oymj/greenearthhero/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.oymj.greenearthhero
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener{
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener{
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
